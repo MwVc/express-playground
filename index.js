bbd8399e53f4e15404a4e886546a55a11df6b6b9
Victor Mwadime <victormwadime@gmail.com>
2024-11-01 13:55:30 +0300

    import environment variable and refactor routing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-
+const port = process.env.PORT || 8000;
 const app = express();
 // app.use(cors());
 
@@ -26,8 +26,24 @@ let posts = [
   { id: 3, title: "Post Three" },
 ];
 
+// Get all posts
 app.get("/api/posts", (req, res) => {
-  res.json(posts);
+  const limit = parseInt(req.query.limit);
+  if (!isNaN(limit) && limit > 0) {
+    return res.status(200).json(posts.slice(0, limit));
+  }
+  res.status(200).json(posts);
 });
 
-app.listen(8080, () => console.log("Server is running on port 8080"));
+// Get single post
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    return res.status(404).send("Server error: Resource not found");
+  }
+  res.status(200).send(post);
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
