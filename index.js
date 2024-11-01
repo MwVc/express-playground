9485f7cd51a4b8318407b135fa4d0f19098ae77e
Victor Mwadime <victormwadime@gmail.com>
2024-11-01 12:04:58 +0300

    create api to send json data using get request

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+// app.use(cors());
 
 // setup static folder
 
@@ -14,5 +16,18 @@ app.use(express.static(path.join(__dirname, "public"))); // middleware - functio
 // app.get("/about", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "about.html"));
 // });
+
+// JSON API
+
+// posts in a database example
+let posts = [
+  { id: 1, title: "Post One" },
+  { id: 2, title: "Post Two" },
+  { id: 3, title: "Post Three" },
+];
+
+app.get("/api/posts", (req, res) => {
+  res.json(posts);
+});
 
 app.listen(8080, () => console.log("Server is running on port 8080"));
