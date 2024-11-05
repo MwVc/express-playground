53ebcb4f117b3ef5dde8a736c04674e90062b43b
Victor Mwadime <victormwadime@gmail.com>
2024-11-05 15:57:48 +0300

    create custom middleware function

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const apiRouter = require("./routes/posts");
 
+function myMiddleWare(req, res, next) {
+  log("Middleware function is called");
+
+  req.customProperty = "Hello from middleware";
+
+  next();
+}
+
+app.use(myMiddleWare);
 app.use("/", indexRouter);
 app.use("/books", bookRouter);
 app.use("/author", authorRouter);
+app.use("/api", apiRouter);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
