8999eb285901859e4dda260bbc0bfe11145ff63d
Victor Mwadime <victormwadime@gmail.com>
2024-11-06 10:47:47 +0300

    add middleware function that handles errors

--- index.js
+++ index.js
@@ -21,4 +21,10 @@ app.use("/books", bookRouter);
 app.use("/author", authorRouter);
 app.use("/api", apiRouter);
 
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
