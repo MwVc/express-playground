206fd61017844fc8ee970be3bc98f05685c2dce3
Victor Mwadime <victormwadime@gmail.com>
2024-11-07 15:06:11 +0300

    create user data and a fetch function to simulate getting data from database

--- data/db.js
+++ data/db.js
@@ -8,4 +8,16 @@ async function getAuthorById(id) {
   return authors.find((author) => author.id === id);
 }
 
-module.exports = { getAuthorById };
+const users = [
+  { id: 1, name: "Mwadime" },
+  { id: 2, name: "Victor" },
+  { id: 3, name: "Kamwana" },
+];
+
+async function getUserById(id) {
+  return users.find((user) => {
+    return user.id === id;
+  });
+}
+
+module.exports = { getAuthorById, getUserById };
