27271d727636fddea10b7b6b805a98e7ccf0f310
Victor Mwadime <victormwadime@gmail.com>
2024-11-07 15:07:09 +0300

    relearn routing

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const port = process.env.PORT || 8000;
 const app = express();
+const authorRouter = require("./routes/authorRouter");
+
+app.get("/", (req, res) => {
+  res.send("This is my homepage");
+});
+
+app.use("/authors", authorRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).send(err);
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
