360d5a887bf0fa12c418c7e84f0bfa5e9179ad78
Victor Mwadime <victormwadime@gmail.com>
2024-11-08 11:55:54 +0300

    update app

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 const userRouter = require("./routes/userRouter");
+const assetsPath = path.join(__dirname, "public");
+
+// Setting up App properties
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Serving static assets
+app.use(express.static(assetsPath));
+
+const links = [
+  { href: "https://axample.com", text: "Example Website" },
+  { href: "https://fakejson.com", text: "Fake JSON" },
+  { href: "https://mwvc.netlify.app", text: "Portfolio website" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
 
 app.get("/", (req, res) => {
-  res.send("This is the app");
+  res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { message: "This is an about page." });
 });
 
 app.use("/users", userRouter);
